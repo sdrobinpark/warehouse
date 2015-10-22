96751a490f2ab0856c5f704118b8178f4ef0c837
sdrobinpark <sdrobinpark@gmail.com>
2015-10-22 11:45:16 -0700

    html5

--- app/controllers/Hello.java
+++ app/controllers/Hello.java
@@ -8,4 +8,7 @@ public class Hello extends Controller{
 	public Result hello(String name) {
 		return ok(hellowelcomepage.render(name));
 	}
+	public Result html5(){
+		return ok(html5.render());
+}
 }
